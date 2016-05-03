9819cf6a0037f41c218e4779e7aa6534130bfefc
Pavan Machavolu <pavan.m@tectoro.com>
2016-05-03 13:29:51 +0530

    Support for latest cordova version

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -157,9 +157,9 @@
 
         // Check what Notifications the user has turned on.  We registered for all three, but they may have manually disabled some or all of them.
         //NSUInteger rntypes = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
-    
+
         #define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
-    
+
         NSUInteger rntypes;
         if (!SYSTEM_VERSION_LESS_THAN(@"8.0")) {
             rntypes = [[[UIApplication sharedApplication] currentUserNotificationSettings] types];
@@ -227,7 +227,7 @@
         NSLog(@"Msg: %@", jsonStr);
 
         NSString * jsCallBack = [NSString stringWithFormat:@"%@(%@);", self.callback, jsonStr];
-        [self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
+        [self.webViewEngine evaluateJavaScript:jsCallBack completionHandler:nil];
 
         self.notificationMessage = nil;
     }
